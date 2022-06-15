b64902ce1007a5991171c7fdcb3419993cc53894
Paulo <paulo.freire53@gmail.com>
2022-06-14 22:28:55 -0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
+import { Header } from "./components/Header";
+import "./global.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <p>You clicked {count} times</p>
+    <div>
+      <Header />
     </div>
   );
 }
